543730bef7f056018a381c5805579035cb2f938e
Astgh78 <astghik.karakhanyan@gmail.com>
2020-07-23 17:17:21 +0400

    making some changes

--- MakingCoffee/src/MakingCoffeeEx1.java
+++ MakingCoffee/src/MakingCoffeeEx1.java
@@ -6,26 +6,32 @@ public class MakingCoffeeEx1 {
             int waterPerCup = 200;
             int milkPerCup = 50;
             int coffeeBeansPerCup = 15;
+            int cupsOfCoffee = 2;
 
             System.out.println("Write how many ml of water the coffee machine has: ");
-            Scanner value1 = new Scanner (System.in);
-            int waterValue = value1.nextInt();
+               System.out.print("> ");
+            Scanner value = new Scanner (System.in);
+            int waterValue = value.nextInt();
 
             System.out.println("Write how many ml of milk the coffee machine has: ");
-            Scanner value2 = new Scanner (System.in);
-            int milkValue = value2.nextInt();
+               System.out.print("> ");
+            int milkValue = value.nextInt();
 
             System.out.println("Write how many grams of coffee beans the coffee machine has: ");
-            Scanner value3 = new Scanner (System.in);
-            int coffeeBeansValue = value3.nextInt();
+               System.out.print("> ");
+            int coffeeBeansValue = value.nextInt();
 
             System.out.println("Write how many cups of coffee you will need: ");
-            Scanner value4 = new Scanner (System.in);
-            int neededCupsOfCoffee = value4.nextInt();
+               System.out.print("> ");
+            int neededCupsOfCoffee = value.nextInt();
 
             if (waterValue % waterPerCup >= neededCupsOfCoffee && milkValue % milkPerCup >= neededCupsOfCoffee && coffeeBeansValue % coffeeBeansPerCup >= neededCupsOfCoffee)
                 System.out.println("Yes, I can make that amount of coffee");
-            else System.out.println("Sorry there are no enough ingredients");
+
+            else {
+                if (waterValue % waterPerCup >= cupsOfCoffee && milkValue % milkPerCup >= cupsOfCoffee && coffeeBeansValue % coffeeBeansPerCup >= cupsOfCoffee)
+                    System.out.println("No, I can make only cupsOfCoffee cup(s) of coffee");
+            }
         }
     }
 
