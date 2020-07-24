2d144ab5cf34db4f72bc217b7ad5f73c93a1ee81
Astgh78 <astghik.karakhanyan@gmail.com>
2020-07-24 16:15:05 +0400

    - added two examples

--- MakingCoffee/src/MakingCoffeeEx1.java
+++ MakingCoffee/src/MakingCoffeeEx1.java
@@ -6,7 +6,8 @@ public class MakingCoffeeEx1 {
             int waterPerCup = 200;
             int milkPerCup = 50;
             int coffeeBeansPerCup = 15;
-            int cupsOfCoffee = 2;
+            int cupsOfCoffee1 = 2;
+               int cupsOfCoffee2 = 5;
 
             System.out.println("Write how many ml of water the coffee machine has: ");
                System.out.print("> ");
@@ -28,10 +29,15 @@ public class MakingCoffeeEx1 {
             if (waterValue % waterPerCup >= neededCupsOfCoffee && milkValue % milkPerCup >= neededCupsOfCoffee && coffeeBeansValue % coffeeBeansPerCup >= neededCupsOfCoffee)
                 System.out.println("Yes, I can make that amount of coffee");
 
-            else {
-                if (waterValue % waterPerCup >= cupsOfCoffee && milkValue % milkPerCup >= cupsOfCoffee && coffeeBeansValue % coffeeBeansPerCup >= cupsOfCoffee)
-                    System.out.println("No, I can make only cupsOfCoffee cup(s) of coffee");
-            }
+            else
+                if (waterValue % waterPerCup >= cupsOfCoffee1 && milkValue % milkPerCup >= cupsOfCoffee1 && coffeeBeansValue % coffeeBeansPerCup >= cupsOfCoffee1)
+                    System.out.println("No, I can make only"  + cupsOfCoffee1 + "cup(s) of coffee");
+
+                else if (waterValue % waterPerCup >= cupsOfCoffee2 && milkValue % milkPerCup >= cupsOfCoffee2 && coffeeBeansValue % coffeeBeansPerCup >= cupsOfCoffee2)
+                   System.out.println("Yes, I can make that amount of coffee (and even 2 more than that)");
+
+
+
         }
     }
 
